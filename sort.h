8d64bf524ea80b75ea1219245d8f27e03a33048f
Partrixs <patricknjoroge092@gmail.com>
2023-11-20 17:58:44 +0300

    C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -33,6 +33,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 /* Sorting algoritms */
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
