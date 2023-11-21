a25b3adab1f977f0c65dad7964b2ec626fb1fae1
Partrixs <patricknjoroge092@gmail.com>
2023-11-21 12:14:30 +0300

    C - Sorting algorithms & Big O

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -5,7 +5,7 @@
 
 #include "sort.h"
 
-void bitonic_merge(int *array, size_t size, size_t start, size_t seq,char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void swap_ints(int *a, int *b);
 void bitonic_sort(int *array, size_t size);
